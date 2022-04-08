3658080fafdd11ec8adc24320746af3e7013aa6a
Olga Bueno Tamayo <58338238+XainaDiGraft@users.noreply.github.com>
2022-04-08 19:35:06 +0200

    modificacion esquema

--- servidorExpress/DatoSchema.js
+++ servidorExpress/DatoSchema.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const DatosSchema = mongoose.Schema({
-    datosPaciente:{
         nombre: {type: String},
         apellido1: {type: String},
         apellido2: {type: String},
@@ -40,115 +39,113 @@ const DatosSchema = mongoose.Schema({
             }
         ],
         permisoGrabacion:{type:Boolean},
-        firmaProteccionDatos:{type:Boolean}
-       
-    },
-    historial:[
-        {
-            antecendentes:[{
-                familiares:[{
-                    observaciones:{type:String},
-                    usu:{type: String},
-                    f_usu:{type: Date},
-                    acion_usu:{type: String}
+        firmaProteccionDatos:{type:Boolean},
+        historial:[
+            {
+                antecendentes:[{
+                    familiares:[{
+                        observaciones:{type:String},
+                        usu:{type: String},
+                        f_usu:{type: Date},
+                        acion_usu:{type: String}
+                    }],
+                    personales:[{
+                        observaciones:{type:String},
+                        usu:{type: String},
+                        f_usu:{type: Date},
+                        acion_usu:{type: String}
+                    }]
                 }],
-                personales:[{
-                    observaciones:{type:String},
-                    usu:{type: String},
-                    f_usu:{type: Date},
-                    acion_usu:{type: String}
-                }]
-            }],
-            procedencia:[
-                {
-                    fecha_inicio:{type: Date},
-                    fecha_alta:{type: Date},
-                    quien_lo_envia:{type: String},
-                    psicologo:{type:String},
-                    motivo_consulta:{type:String},
-                    sintomas:{type:String},
-                    usu:{type: String},
-                    f_usu:{type: Date},
-                    acion_usu:{type: String},
-                    diagnostico_psicologico:[
-                        {
-                            diagnostico:{type: String},
-                            fechaDiagnostico:{type:Date},
-                            observaciones_personales:{type: String},
-                            usu:{type: String},
-                            f_usu:{type: Date},
-                            acion_usu:{type: String}
-                        }
-                    ],          
-                    diagnostico_medico:[
-                        {
-                            patologiaMedica:{type:String} ,
-                            fechaDiagnostico:{type:Date},
-                            tratamiento:[{
-                                medicamento:{type:String} ,
-                                fechaInicio:{type:Date},
-                                fechaFin:{type:Date},
-                                posologia:{
-                                    desayuno:{type: Number},
-                                    comida:{type: Number},
-                                    cena:{type: Number},
-                                    otros:{type: Number}
-                                },
-                                observaciones:{type:String}
-                            }],
-                            usu:{type: String},
-                            f_usu:{type: Date},
-                            acion_usu:{type: String}                  
-                        },
-                    ],
-                    
-                    valoraciones: [
-                        {   
-                            cognitiva:[{
-                                fecha_valoracion:{type:Date},
-                                observaciones:{type:String},
-                                usu:{type: String},
-                                f_usu:{type: Date},
-                                acion_usu:{type: String} 
-                            }],
-                            emocional:[{
-                                fecha_valoracion:{type:Date},
-                                observaciones:{type:String},
+                consulta:[
+                    {
+                        procedencia:{type: String},
+                        fecha_inicio:{type: Date},
+                        fecha_alta:{type: Date},
+                        procedencia:{type: String},
+                        psicologo:{type:String},
+                        motivo_consulta:{type:String},
+                        sintomas:{type:String},
+                        usu:{type: String},
+                        f_usu:{type: Date},
+                        acion_usu:{type: String},
+                        diagnostico_psicologico:[
+                            {
+                                diagnostico:{type: String},
+                                fechaDiagnostico:{type:Date},
+                                observaciones_personales:{type: String},
                                 usu:{type: String},
                                 f_usu:{type: Date},
                                 acion_usu:{type: String}
-                            }],
-                            pruebasPsicodiagnostico:[{
-                                fecha_prueba:{type:Date},
-                                observaciones:{type:String},
+                            }
+                        ],          
+                        diagnostico_medico:[
+                            {
+                                patologiaMedica:{type:String} ,
+                                fechaDiagnostico:{type:Date},
+                                tratamiento:[{
+                                    medicamento:{type:String} ,
+                                    fechaInicio:{type:Date},
+                                    fechaFin:{type:Date},
+                                    posologia:{
+                                        desayuno:{type: Number},
+                                        comida:{type: Number},
+                                        cena:{type: Number},
+                                        otros:{type: Number}
+                                    },
+                                    observaciones:{type:String}
+                                }],
                                 usu:{type: String},
                                 f_usu:{type: Date},
-                                acion_usu:{type: String}
-                            }]
+                                acion_usu:{type: String}                  
+                            },
+                        ],
+                        
+                        valoraciones: [
+                            {   
+                                cognitiva:[{
+                                    fecha_valoracion:{type:Date},
+                                    observaciones:{type:String},
+                                    usu:{type: String},
+                                    f_usu:{type: Date},
+                                    acion_usu:{type: String} 
+                                }],
+                                emocional:[{
+                                    fecha_valoracion:{type:Date},
+                                    observaciones:{type:String},
+                                    usu:{type: String},
+                                    f_usu:{type: Date},
+                                    acion_usu:{type: String}
+                                }],
+                                pruebasPsicodiagnostico:[{
+                                    fecha_prueba:{type:Date},
+                                    observaciones:{type:String},
+                                    usu:{type: String},
+                                    f_usu:{type: Date},
+                                    acion_usu:{type: String}
+                                }]
 
-                        }
+                            }
+                    
+                        ],
+                        seguimiento: [
+                            {   
+                                fecha_cita:{type:Date},
+                                hora_inicio_cita:{type:String},
+                                hora_fin_cita:{type:String},
+                                observaciones:{type:String},
+                                fecha_prox_cita:{type:Date},
+                                hora_prox_cita:{type:String},
+                                conducta_a_seguir:{type:String},
+                                usu:{type: String},
+                                f_usu:{type: Date},
+                                acion_usu:{type: String}  
+                            }
+                        ]
+                    }   
                 
-                    ],
-                    seguimiento: [
-                        {   
-                            fecha_cita:{type:Date},
-                            hora_inicio_cita:{type:String},
-                            hora_fin_cita:{type:String},
-                            observaciones:{type:String},
-                            fecha_prox_cita:{type:Date},
-                            hora_prox_cita:{type:String},
-                            conducta_a_seguir:{type:String},
-                            usu:{type: String},
-                            f_usu:{type: Date},
-                            acion_usu:{type: String}  
-                        }
-                    ]
-                }   
-             
-            ]
-        }
-    ]
-   
+                ]
+            }
+        ]
 })
 
 module.exports = mongoose.model('Formato', DatosSchema);
