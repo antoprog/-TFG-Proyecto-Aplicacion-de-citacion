aef3ee43927ce9fa22dd7ae3660b27793fa6b593
Edward <george.wkp@gmail.com>
2022-04-13 21:29:04 +0200

    Primer commit

--- servidorExpress/nombreClientes.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const clienteSchema = mongoose.Schema({
-    name: {
-        type: String
-    }
-})
-
-module.exports = mongoose.model('nombresClientes', clienteSchema);
--- servidorExpress/usuario.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = mongoose.Schema({
-    user: String,
-    pass: String
-})
-
-module.exports = mongoose.model('usuarios', UserSchema);
