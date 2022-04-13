ef4ff20affafb4f1b5cfa46b7518873f39a658c9
Olga Bueno Tamayo <58338238+XainaDiGraft@users.noreply.github.com>
2022-04-13 20:02:07 +0200

    Actualizar app-routing.module.ts y botonera.component.html

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AltaPacienteComponent } from './componentes/cuerpo/contenido/alta-paciente/alta-paciente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"/administracion/altaPaciente",
+    component:AltaPacienteComponent
+    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
