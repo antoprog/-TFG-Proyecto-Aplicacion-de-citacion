b822500953ba3e2a0d4a8eafb5a4866c7400898b
Olga Bueno Tamayo <58338238+XainaDiGraft@users.noreply.github.com>
2022-04-14 14:16:20 +0200

    Actualizar Psicologo.js

---
+++ servidorBabel/src/models/Psicologo.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const DatosSchema = mongoose.Schema({
+    nombre: {type: String},
+    apellido1: {type: String},
+    apellido2: {type: String},
+    tipo_doc: {type: String},
+    documento: {type: String},
+    titulacion:{type:String},
+    especialidad:{type:String},
+    credenciales_adic:{type:String},
+    num_colegiado:{type:String},
+    telefono: {type: String},
+    email: {type: String},    
+    direccion: {
+        calle: {type: String},
+        cod_postal: {type: String},
+        ciudad: {type: String},
+        provincia: {type: String},
+        pais: {type: String},
+        usu: {type: String},
+        f_usu: {type: Date},
+        acion_usu: {type: String}
+    },
+})
+module.exports = mongoose.model('Psicologo', DatosSchema);
