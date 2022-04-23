f418eb63f2c1eb1365ae5acb887b28052f651712
Edward <george.wkp@gmail.com>
2022-04-23 21:53:25 +0200

    Actualizar signin.component.ts

--- cliente/src/app/componentes/login/signin/signin.component.ts
+++ cliente/src/app/componentes/login/signin/signin.component.ts
@@ -1,34 +1,41 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
 import {AuthService} from "../../../servicios/auth.service";
+import {ToastrService} from "ngx-toastr";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+    selector: 'app-signin',
+    templateUrl: './signin.component.html',
+    styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-  user = {
-    username: '',
-    password: ''
-  }
+    user = {
+        username: '',
+        password: ''
+    }
 
-  constructor(private servicio: AuthService,
-              private router: Router) {
-  }
+    constructor(private servicio: AuthService,
+                private toastr: ToastrService,
+                private router: Router) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  signIn() {
-    this.servicio.singin(this.user)
-      .subscribe(
-        res => {
-          console.log('Login:', res.token);
-          localStorage.setItem('token', res.token);
-          window.location.reload();
-        },
-        error => console.log(error.status, error.error.message)
-      )
-  }
+    signIn() {
+        this.servicio.singin(this.user)
+            .subscribe(
+                res => {
+                    this.toastr.error('', "Login bien")
+                    console.log('Login:', res.token);
+                    localStorage.setItem('token', res.token);
+                    this.router.navigate(['.']).then();
+                   // window.location.reload();
+                },
+                error => {
+                    console.log(error.status, error.error.message)
+                    this.toastr.error('', "Los datos son incorrectos.")
+                }
+            )
+    }
 }
