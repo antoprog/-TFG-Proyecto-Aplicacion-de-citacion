0eecace145e4b3a3fc724e17797029ff5f58c4f9
edward <george.wkp@gmail.com>
2022-05-10 13:32:16 +0200

    Actualizar package-lock.json, seguimiento.component.html y 2 más archivos...

--- cliente/src/app/componentes/cuerpo/contenido/seguimiento/seguimiento.component.ts
+++ cliente/src/app/componentes/cuerpo/contenido/seguimiento/seguimiento.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder} from '@angular/forms';
 import {BbddService} from 'src/app/servicios/bbdd.service';
-import { DataShareService } from 'src/app/servicios/data-share.service';
+import {DataShareService} from 'src/app/servicios/data-share.service';
+import {DatePipe} from "@angular/common";
+import {Paciente} from "../../../../modelo/paciente";
+import * as moment from "moment";
 
 @Component({
     selector: 'app-seguimiento',
@@ -9,18 +12,15 @@ import { DataShareService } from 'src/app/servicios/data-share.service';
     styleUrls: ['./seguimiento.component.css']
 })
 export class SeguimientoComponent implements OnInit {
-// this.seguimientoForm.controls['procedencia'].setValue(value.datosMedicos.valoracion[_valoracion].procedencia)
-// this.seguimientoForm.controls['fecha_diagnostico'].setValue(String(value.datosMedicos.valoracion[_valoracion].diagnostico_medico.fecha_diagnostico).split('T')[0])
-// this.seguimientoForm.controls['con_motivo'].setValue(value.datosMedicos.valoracion[_valoracion].motivo_consulta)
-// this.seguimientoForm.controls['con_sintomas'].setValue(value.datosMedicos.valoracion[_valoracion].sintomas)
-
-constructor(private fb: FormBuilder,
-    private bbdd: BbddService,
-    private dataShare: DataShareService) { 
+    constructor(private fb: FormBuilder,
+                private bbdd: BbddService,
+                private dataShare: DataShareService,
+                private pipedate: DatePipe) {
 
     }
-    
+
     sus2: any
+
     ngOnInit(): void {
         this.cargarPantalla();
     }
@@ -29,7 +29,8 @@ constructor(private fb: FormBuilder,
         this.sus2.unsubscribe();
     }
 
-    datos: any
+    tablaSeguimientos: String[] = []
+    datosPaciente: any
 
     seguimientoForm = this.fb.group({
         observaciones_cita: [''],
@@ -40,33 +41,63 @@ constructor(private fb: FormBuilder,
         fin_p_cita: ['']
     })
 
-    
-cargarPantalla() {
-    this.sus2 = this.dataShare.paciente$.subscribe(
-        {
-            next: value => {
-                if (value) {
 
-                    const ruta = value.datosMedicos?.valoracion[parseInt(localStorage.getItem('valoracionId')!)]
-                    this.seguimientoForm.controls['observaciones_cita'].setValue(ruta?.seguimiento.observaciones)
-                    this.seguimientoForm.controls['observaciones_propias'].setValue(ruta?.seguimiento.anotaciones)
-                    this.seguimientoForm.controls['conducta_a_seguir'].setValue(ruta?.seguimiento.conducta_a_seguir)
-                    this.seguimientoForm.controls['fecha_p_cita'].setValue(ruta?.seguimiento.fecha_prox_cita)
-                    this.seguimientoForm.controls['inicio_p_cita'].setValue(ruta?.seguimiento.hora_inicio_cita)
-                    this.seguimientoForm.controls['fin_p_cita'].setValue(ruta?.seguimiento.hora_fin_cita)
+    cargarPantalla() {
+        this.sus2 = this.dataShare.paciente$.subscribe(
+            {
+                next: value => {
+                    if (value) {
+                        const ruta = value.datosMedicos?.valoracion[parseInt(localStorage.getItem('valoracionId')!)]
+                        // this.seguimientoForm.controls['observaciones_cita'].setValue(ruta?.seguimiento.observaciones)
+                        // this.seguimientoForm.controls['observaciones_propias'].setValue(ruta?.seguimiento.anotaciones)
+                        // this.seguimientoForm.controls['conducta_a_seguir'].setValue(ruta?.seguimiento.conducta_a_seguir)
+                        // this.seguimientoForm.controls['fecha_p_cita'].setValue(ruta?.seguimiento.fecha_prox_cita)
+                        // this.seguimientoForm.controls['inicio_p_cita'].setValue(ruta?.seguimiento.hora_inicio_cita)
+                        // this.seguimientoForm.controls['fin_p_cita'].setValue(ruta?.seguimiento.hora_fin_cita)
 
+                        this.datosPaciente = ruta
+
+                        let fechaFormateada: string | null;
+
+                        console.log(ruta.seguimiento.length);
+
+                        this.tablaSeguimientos = []
+                        for (const rutaElement of ruta.seguimiento) {
+                            if (rutaElement.fecha_cita !== undefined) {
+                                fechaFormateada = this.pipedate.transform(rutaElement.fecha_cita, 'dd-MM-yyyy');
+                            }
+                            this.tablaSeguimientos.push(fechaFormateada!)
+                        }
+
+                        this.cargarDatos(ruta.seguimiento[ruta.seguimiento.length - 1])
+                    }
                 }
             }
-        }
-    )
-  }
+        )
+    }
 
     guardar() {
         this.bbdd.modificarSeguimiento(this.seguimientoForm.value).subscribe()
-      
+
         setTimeout(() => {
             this.dataShare._idPaciente$.next(String(localStorage.getItem('idPaciente')))
         }, 1500)
     }
 
+    cambiarValoracion(evento: any) {
+        for (const seguimiento of this.datosPaciente.seguimiento) {
+            if (evento.value === this.pipedate.transform(seguimiento.fecha_cita, 'dd-MM-yyyy')){
+                this.cargarDatos(seguimiento)
+            }
+        }
+    }
+
+    cargarDatos(seguimiento:any){
+        this.seguimientoForm.controls['observaciones_cita'].setValue(seguimiento.observaciones)
+        this.seguimientoForm.controls['observaciones_propias'].setValue(seguimiento.anotaciones)
+        this.seguimientoForm.controls['conducta_a_seguir'].setValue(seguimiento.conducta_a_seguir)
+        this.seguimientoForm.controls['fecha_p_cita'].setValue(seguimiento.fecha_prox_cita)
+        this.seguimientoForm.controls['inicio_p_cita'].setValue(seguimiento.hora_inicio_cita)
+        this.seguimientoForm.controls['fin_p_cita'].setValue(seguimiento.hora_fin_cita)
+    }
 }
